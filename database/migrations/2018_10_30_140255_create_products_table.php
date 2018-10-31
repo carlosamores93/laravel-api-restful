87ed06a17dc65b6b96c99e1e7280e8144b965b53
carlosamores <amorescarlos93@hotmail.com>
2018-10-31 23:56:20 +0100

    Migrations tables ;)

--- database/migrations/2018_10_30_140255_create_products_table.php
+++ database/migrations/2018_10_30_140255_create_products_table.php
@@ -15,6 +15,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('detail');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
